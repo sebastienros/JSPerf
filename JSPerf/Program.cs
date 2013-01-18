e3f74ccae2837400a0598cd613e11b7c04ecb5b6
Sébastien Ros <sebastienros@gmail.com>
2013-01-18 15:23:42 -0800

    Initial commit

---
+++ JSPerf/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace JSPerf
+{
+    class Program
+    {
+        private const string script = @"
+            var o = {};
+            o.Foo = 'bar';
+            o.Baz = 42;
+            o.Blah = o.Foo + o.Baz;
+        ";
+
+        static void Main(string[] args)
+        {
+            const int iterations = 1000;
+            const bool reuseEngine = false;
+
+            var watch = new Stopwatch();
+
+            var jint = new Jint.JintEngine();
+            
+            watch.Restart();
+            for (var i = 0; i < iterations; i++)
+            {
+                if (!reuseEngine)
+                {
+                    jint = new Jint.JintEngine();
+                }
+
+                jint.Run(script);
+            }
+
+            Console.WriteLine("Jint: {0} iterations in {1} ms", iterations, watch.ElapsedMilliseconds);
+            
+            var jurassic = new Jurassic.ScriptEngine();
+
+            watch.Restart();
+            for (var i = 0; i < iterations; i++)
+            {
+                if (!reuseEngine)
+                {
+                    jurassic = new Jurassic.ScriptEngine();
+                }
+
+                jurassic.Execute(script);
+            }
+
+            Console.WriteLine("Jurassic: {0} iterations in {1} ms", iterations, watch.ElapsedMilliseconds);
+
+        }
+    }
+}
